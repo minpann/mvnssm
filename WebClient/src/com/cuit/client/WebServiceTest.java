ff1ab935173d388918a6cea7f834894d2d041f48
minpan <705450732@qq.com>
2016-04-18 22:05:31 +0800

    ssmdemo

--- WebClient/src/com/cuit/client/WebServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.cuit.client;
-
-public class WebServiceTest {
-	public static void main(String[] args) {
-		Server service = new ServerService().getServerPort();
-		System.out.println(service.getValue("王明"));
-		System.out.println(service.reverseString("qwerty"));
-	}
-}
--- WebClient/src/com/cuit/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.cuit.com/")
-package com.cuit.client;
