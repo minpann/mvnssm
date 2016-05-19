1e1fb22dffc48aca4ba99de204f19bb2df631645
pan.min <pan.min@tcl.com>
2016-05-19 09:39:26 +0800

    添加问价上传功能

--- mvnssm/src/main/java/com/cuit/mvnssm/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.cuit.mvnssm.service;
-
-import com.cuit.mvnssm.pojo.StudentEntity;
-
-public interface StudentService{
-
-	StudentEntity selectById(int stuId);
-
-}
--- mvnssm/src/main/java/com/cuit/mvnssm/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.cuit.mvnssm.service;
-
-import com.cuit.mvnssm.pojo.User;
-
-public interface UserService {
-	public User getUserById(int id);
-}
